da0886c6aef885dc7b931e9b8d562b7efd88530b
Connor Hansen <chansen13@georgefox.edu>
2021-12-01 09:46:56 -0800

    implement redux

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,82 +1,27 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+
 // import PropTypes from "prop-types";
 
-
-
-// Error: Objects are not valid as a React child (found: object with keys {boardJsx}).
-//  If you meant to render a collection of children, use an array instead.
-
 class Game extends React.Component {
-  // function Game() {
-
-
-  constructor(props) {
-    super(props)
-
-    const initialBoard = [];
-    for (let i = 0; i < 10; i++) {
-      let tempRow = []
-      for (let j = 0; j < 10; j++) {
-        //make an array of 10 0's
-        const isBomb = Math.random() < .15;
-        tempRow.push({
-          bombs: isBomb ? 'B' : 0,
-          id: i.toString() + j.toString(),
-          row: i,
-          col: j,
-          hidden: true
-        });
-      }
-      initialBoard.push(tempRow)
-    }
-
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        // console.log(`i: ${i}, j: ${j}`);
-        if (initialBoard[i][j].bombs !== 'B') {
-          // Top row - k is the column num
-          for (let k = j - 1; k <= j + 1; k++) {
-            if (i - 1 >= 0 && i - 1 < 10 && k >= 0 && k < 10 && initialBoard[i - 1][k].bombs === 'B') {
-              initialBoard[i][j].bombs++
-            }
-          }
-          // left tile
-          if (i >= 0 && i < 10 && j - 1 >= 0 && j - 1 < 10 && initialBoard[i][j - 1].bombs === 'B') {
-            initialBoard[i][j].bombs++
-          }
-          // right tile
-          if (i >= 0 && i < 10 && j + 1 >= 0 && j + 1 < 10 && initialBoard[i][j + 1].bombs === 'B') {
-            initialBoard[i][j].bombs++
-          }
-          // Bottom row - k is the column num
-          for (let k = j - 1; k <= j + 1; k++) {
-            if (i + 1 >= 0 && i + 1 < 10 && k >= 0 && k < 10 && initialBoard[i + 1][k].bombs === 'B') {
-              initialBoard[i][j].bombs++
-            }
-          }
-        }
-      }
-    }
-    this.state = {
-      board: initialBoard
-    }
-  }
-
   // const [board, setBoard] = React.useState(initialBoard);
 
   tileClick = (tile) => {
+    const { dispatch } = this.props;
+    //this.props.dispatch(action)
     if (tile.bombs === "B") {
       alert('bomb! you lose');
     }
     console.dir(tile)
-
-    
+    //create an action, pass to dispatch
+    //use dispatch similarly to setState
+    const action = { type: "SHOW_TILE", row: tile.row, col: tile.col };
+    dispatch(action);
   }
   //inline option: () => alert(`clicked tile ${tile.row}, ${tile.col}`)
-
+  
   render() {
-    const boardJsx = this.state.board.map((row, i) => {
+    const boardJsx = this.props.board.map((row, i) => {
       return <div className="board-row" key={i}>{row.map(tile => {
         return <div key={tile.id} className="tile" onClick={() => this.tileClick(tile)}>{tile.hidden ? "" : tile.bombs}</div>
       })}</div>
@@ -85,4 +30,16 @@ class Game extends React.Component {
   }
 }
 
+//
+const mapStateToProps = state => {
+  //return state ?
+  return {
+    board: state.board
+  }
+}
+
+Game = connect(mapStateToProps)(Game);
+//connect lets us access the state from store from provider
+//state comes in, we "map" it to our props
+//now we can use that as this.props.... inside this component
 export default Game;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import boardReducer from './reducers/board-reducer';
+import { Provider } from 'react-redux';
+
+const initialBoard = [];
+for (let i = 0; i < 10; i++) {
+  let tempRow = []
+  for (let j = 0; j < 10; j++) {
+    //make an array of 10 0's
+    const isBomb = Math.random() < .15;
+    tempRow.push({
+      bombs: isBomb ? 'B' : 0,
+      id: i.toString() + j.toString(),
+      row: i,
+      col: j,
+      hidden: true
+    });
+  }
+  initialBoard.push(tempRow)
+}
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    // console.log(`i: ${i}, j: ${j}`);
+    if (initialBoard[i][j].bombs !== 'B') {
+      // Top row - k is the column num
+      for (let k = j - 1; k <= j + 1; k++) {
+        if (i - 1 >= 0 && i - 1 < 10 && k >= 0 && k < 10 && initialBoard[i - 1][k].bombs === 'B') {
+          initialBoard[i][j].bombs++
+        }
+      }
+      // left tile
+      if (i >= 0 && i < 10 && j - 1 >= 0 && j - 1 < 10 && initialBoard[i][j - 1].bombs === 'B') {
+        initialBoard[i][j].bombs++
+      }
+      // right tile
+      if (i >= 0 && i < 10 && j + 1 >= 0 && j + 1 < 10 && initialBoard[i][j + 1].bombs === 'B') {
+        initialBoard[i][j].bombs++
+      }
+      // Bottom row - k is the column num
+      for (let k = j - 1; k <= j + 1; k++) {
+        if (i + 1 >= 0 && i + 1 < 10 && k >= 0 && k < 10 && initialBoard[i + 1][k].bombs === 'B') {
+          initialBoard[i][j].bombs++
+        }
+      }
+    }
+  }
+}
+
+const store = createStore(boardReducer, { board: initialBoard });
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
